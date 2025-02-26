d50c35ec53f2a97d39577b79da41bc4e9a5b4a42
Lê Thị Huyền Trang <119200256+ngvqphuc2610@users.noreply.github.com>
2025-02-26 16:22:37 +0700

    Add files via upload

---
+++ bmttnangcao/lab-01/ex04/Main.py
@@ -0,0 +1,10 @@
+from QuanLySinhVien import QuanLySinhVien 
+
+qlsv = QuanLySinhVien()
+while (1==1):
+    print("CHUONG TRINH QUAN LY SINH VIEN")
+    print("***********MENU***************")
+    print("1.Them sinh vien")
+    print("2.Cap nhat thong tin sinh vien boi ID")
+    print("3.Xoa sinh vien boi ID")
+    print("4")
